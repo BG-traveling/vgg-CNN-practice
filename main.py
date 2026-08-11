50aa28cacad7dbcb75c0bbfa8e7f6f49421809fc
dhkim <hades010101@naver.com>
2026-08-11 12:13:23 +0900

    완성

--- main.py
+++ main.py
@@ -22,6 +22,10 @@ from Utils.DataLoader import get_dataloader
 from models.vgg import get_vgg_model
 import train #파일 전체를 import해서 함수를 쓸 때 소속을 train. 으로 시작
 from eval import evaluate
+from Utils.graph import draw_plot
+from models.resnet_pre import get_resnet_model
+
+from tqdm import tqdm
 
 def run_epoch(model, history, EPOCHS=10):
 
@@ -36,7 +40,9 @@ def run_epoch(model, history, EPOCHS=10):
 
     best_acc = 0.0
 
-    for e in range(EPOCHS):
+    prograss_bar = tqdm(range(1, EPOCHS+1))
+
+    for e in prograss_bar:
             #평균 loss, 맞춘 비율 %
             #model, loader, criterion, optimizer, device
             train_loss, train_acc = train.train_one_epoch(model, 
@@ -53,13 +59,16 @@ def run_epoch(model, history, EPOCHS=10):
             history['train_acc'].append(train_acc)
             history['valid_loss'].append(valid_loss)
             history['valid_acc'].append(valid_acc)
+            prograss_bar.set_postfix({'train_loss':f'{train_loss:.2f}',
+                                      'train_acc':f'{train_acc:.2f}',
+                                      'valid_loss':f'{valid_loss:.2f}',
+                                      'valid_acc':f'{valid_acc:.2f}'})
     
             if valid_acc > best_acc :
                 best_acc = valid_acc
                 save_path = f'./CIFAR10_VGG11_{e+1}epoch_{valid_acc:.2f}.pth' 
                 torch.save(model.state_dict(), save_path)
-    
-                print(f'최고 기록 ! {e+1}회 에포크 --> {valid_acc:.2f}')
+                tqdm.write(f'최고 기록 갱신! -> {e+1}에포크: {valid_acc:.2f}')
     return history
     
 
@@ -68,8 +77,11 @@ if __name__ == '__main__':
     train_loader, valid_loader = get_dataloader()
 
     #vgg11모델과 가중치를 로딩하는 함수
-    model = get_vgg_model()
+    model = get_resnet_model()
+    print(model)
     history = {'train_loss':[], 'train_acc':[], 'valid_loss':[], 'valid_acc':[]}
 
     #model_history 훈련이 끝났을 때의 총 history를 반환
-    model_history = run_epoch(model, history, EPOCHS=50)
+    model_history = run_epoch(model, history, EPOCHS=5)
+    save_path = r'C:\kdh\머신러닝, 딥러닝\로컬1일차_샘플\image\result.jpg'
+    model_graph = draw_plot(model_history, save_path)
